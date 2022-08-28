e246166ac24d6bb771a2ec7405fba21eb4665293
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-08-28 19:52:25 +0900

    main.dart修正

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'blocs/bloc_exports.dart';
+import 'models/task.dart';
 import 'screens/tasks_screen.dart';
 
 void main() {
@@ -17,7 +18,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return BlocProvider(
-      create: (context) => TasksBloc(),
+      create: (context) => TasksBloc()
+        ..add(
+          AddTask(
+            task: Task(title: "タスク1", isDone: null, isDeleted: null),
+          ),
+        ),
       child: MaterialApp(
         title: 'Flutter Tasks App',
         theme: ThemeData(
