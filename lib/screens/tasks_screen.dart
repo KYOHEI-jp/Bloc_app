c11b3692459b64ad2ef7c0704b7f2b5e5682de51
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-08-28 19:52:18 +0900

    tasksScreen修正

--- lib/screens/tasks_screen.dart
+++ lib/screens/tasks_screen.dart
@@ -1,46 +1,46 @@
 import 'package:flutter/material.dart';
 
+import '../blocs/bloc_exports.dart';
 import '../models/task.dart';
 
 class TasksScreen extends StatelessWidget {
   TasksScreen({Key? key}) : super(key: key);
 
-  List<Task> tasksList = [
-    Task(title: "タスク1", isDeleted: null, isDone: null),
-    Task(title: "タスク2", isDeleted: null, isDone: null),
-    Task(title: "タスク3", isDeleted: null, isDone: null),
-  ];
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Tasks App'),
-        actions: [
-          IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.add),
-          )
-        ],
-      ),
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          const Center(
-            child: Chip(
-              label: Text(
-                'Tasks:',
-              ),
-            ),
+    return BlocBuilder<TasksBloc, TasksState>(
+      builder: (context, state) {
+        List<Task> tasksList = state.allTasks;
+        return Scaffold(
+          appBar: AppBar(
+            title: const Text('Tasks App'),
+            actions: [
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(Icons.add),
+              )
+            ],
           ),
-          TasksList(tasksList: tasksList)
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        tooltip: 'Add Task',
-        child: const Icon(Icons.add),
-      ),
+          body: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const Center(
+                child: Chip(
+                  label: Text(
+                    'Tasks:',
+                  ),
+                ),
+              ),
+              TasksList(tasksList: tasksList)
+            ],
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {},
+            tooltip: 'Add Task',
+            child: const Icon(Icons.add),
+          ),
+        );
+      },
     );
   }
 }
