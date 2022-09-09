3cb6ae52c16d4ba47424865bb1f0289eb6d92082
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-09-09 19:57:18 +0900

    hydrated_bloc

--- lib/blocs/bloc/tasks_bloc.dart
+++ lib/blocs/bloc/tasks_bloc.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 
-import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_tasks_app/blocs/bloc_exports.dart';
 
 import '../../models/task.dart';
 
@@ -9,7 +9,7 @@ part 'tasks_event.dart';
 
 part 'tasks_state.dart';
 
-class TasksBloc extends Bloc<TasksEvent, TasksState> {
+class TasksBloc extends HydratedBloc<TasksEvent, TasksState> {
   TasksBloc() : super(const TasksState()) {
     on<AddTask>(_onAddTask);
     on<UpdateTask>(_onUpdateTask);
@@ -28,12 +28,24 @@ class TasksBloc extends Bloc<TasksEvent, TasksState> {
   FutureOr<void> _onUpdateTask(UpdateTask event, Emitter<TasksState> emit) {
     final state = this.state;
     final task = event.task;
+    final int index = state.allTasks.indexOf(task);
 
     List<Task> allTasks = List.from(state.allTasks)..remove(task);
     task.isDone == false
-        ? allTasks.add(task.copyWith(isDone: true))
-        : allTasks.add(task.copyWith(isDone: false));
+        ? allTasks.insert(index, task.copyWith(isDone: true))
+        : allTasks.insert(index, task.copyWith(isDone: false));
+    emit(TasksState(allTasks: allTasks));
   }
 
   FutureOr<void> _onDeleteTask(DeleteTask event, Emitter<TasksState> emit) {}
+
+  @override
+  TasksState? fromJson(Map<String, dynamic> json) {
+    return TasksState.fromMap(json);
+  }
+
+  @override
+  Map<String, dynamic>? toJson(TasksState state) {
+    return state.toMap();
+  }
 }
