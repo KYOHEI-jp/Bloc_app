64290ebf9f6bc1095ba22a7967b491a5c674ad54
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-09-09 20:59:46 +0900

    idをコンストラクター追加

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -3,10 +3,15 @@ import 'package:equatable/equatable.dart';
 // ignore must_be_immutable
 class Task extends Equatable {
   final String title;
+  final String id;
   bool? isDone;
   bool? isDeleted;
 
-  Task({required this.title, required this.isDone, required this.isDeleted}) {
+  Task(
+      {required this.title,
+      required this.id,
+      required this.isDone,
+      required this.isDeleted}) {
     //イニシャライズ
     isDone = isDone ?? false;
     isDeleted = isDeleted ?? false;
@@ -14,23 +19,30 @@ class Task extends Equatable {
 
   Task copyWith({
     String? title,
+    String? id,
     bool? isDone,
     bool? isDeleted,
   }) {
     return Task(
         title: title ?? this.title,
+        id: id ?? this.id,
         isDone: isDone ?? this.isDone,
         isDeleted: isDeleted ?? this.isDeleted);
+  }
+
+  Map<String, dynamic> toMap() {
+    return {'title': title, 'id': id, 'isDone': isDone, 'isDeleted': isDeleted};
   }
 
   factory Task.fromMap(Map<String, dynamic> map) {
     return Task(
       title: map['title'] ?? '',
+      id: map['id'] ?? '',
       isDone: map['isDone'],
       isDeleted: null,
     );
   }
 
   @override
-  List<Object?> get props => [title, isDone, isDeleted];
+  List<Object?> get props => [title, id, isDone, isDeleted];
 }
