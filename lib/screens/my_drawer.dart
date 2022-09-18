ace936f80cf471f26fd957c3bab4a19b1122313c
KYOHEI-jp <kingdomhearts510@gmail.com>
2022-09-18 13:48:54 +0900

    Drawer

--- lib/screens/my_drawer.dart
+++ lib/screens/my_drawer.dart
@@ -11,44 +11,49 @@ class MyDrawer extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Drawer(
-          child: Column(
-        children: [
-          Container(
-            width: double.infinity,
-            padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
-            color: Colors.grey,
-            child: Text(
-              "Task Drawer",
-              style: Theme.of(context).textTheme.headline5,
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: EdgeInsets.symmetric(vertical: 15.0, horizontal: 20.0),
+              color: Colors.grey,
+              child: Text(
+                "Task Drawer",
+                style: Theme.of(context).textTheme.headline5,
+              ),
             ),
-          ),
-          BlocBuilder<TasksBloc, TasksState>(
-            builder: (context, state) {
-              return GestureDetector(
-                onTap: () {
-                  Navigator.of(context).pushNamed(TasksScreen.id);
-                },
-                child: ListTile(
-                  leading: const Icon(Icons.folder_special),
-                  title: const Text("My Tasks"),
-                  trailing: Text("${state.allTasks.length}"),
-                ),
-              );
-            },
-          ),
-          const Divider(),
-          GestureDetector(
-            onTap: () {
-              Navigator.of(context).pushNamed(RecycleBin.id);
-            },
-            child: const ListTile(
-              leading: Icon(Icons.delete),
-              title: Text("Bin"),
-              trailing: Text("0"),
+            BlocBuilder<TasksBloc, TasksState>(
+              builder: (context, state) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pushNamed(TasksScreen.id);
+                  },
+                  child: ListTile(
+                    leading: const Icon(Icons.folder_special),
+                    title: const Text("My Tasks"),
+                    trailing: Text("${state.allTasks.length}"),
+                  ),
+                );
+              },
             ),
-          ),
-        ],
-      )),
+            const Divider(),
+            BlocBuilder<TasksBloc, TasksState>(
+              builder: (context, state) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.of(context).pushNamed(RecycleBin.id);
+                  },
+                  child: ListTile(
+                    leading: Icon(Icons.delete),
+                    title: const Text("Bin"),
+                    trailing: Text('${state.removedTasks.length}'),
+                  ),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
